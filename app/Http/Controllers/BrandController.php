04db0dce733d6e2c868fe94b894ded649972baa9
Muhammad Taha <imtaha999@hotmail.com>
2022-08-29 01:04:13 +0500

    fetching brand details

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class BrandController extends Controller
 {
-    //
+    public function index()
+    {
+        $data = Brand::all();
+        return view('admin.brands.brand', ['brand' => $brand]);
+    }
 }
