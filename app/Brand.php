7a25f9e2e48853154406b7dbecdb6f6899fd1a36
Muhammad-Taha07 <imtaha999@hotmail.com>
2022-12-25 19:40:54 +0500

    Brand Model Added

--- app/Brand.php
+++ app/Brand.php
@@ -7,9 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Brand extends Model
 {
     protected $table = 'brands';
-    protected $fillable = ['brand_name', 'brand_description'];
+    protected $fillable = ['brand_name', 'brand_description', 'created_at', 'updated_at'];
 
+    public function createBrandMethod($data)
+    {
+        $createBrand = Brand::create($data);
+        return $createBrand ? $createBrand :  array();
+    }
 
+    
 
     
 
