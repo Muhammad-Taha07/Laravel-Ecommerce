831721530dbd8f492ce435ae412562deb5a8afd0
MuhammadTaha <muhammad.taha@code-avenue.com>
2023-04-02 23:34:13 +0500

    Unnessary routes removed

--- routes/web.php
+++ routes/web.php
@@ -59,25 +59,4 @@ Route::prefix('product')->group(function() {
 });
     });
 
-// /* Category Routes */
-//     Route::prefix('category')->group(function () {
-//         // Route::get('/view', 'CategoryController@fetchCategories')->name('View_category');
-//         Route::get('/view', [CategoryController::class,'fetchCategories'])->name('View_category');
-//         Route::get('/add', [CategoryController::class, 'AddCategoryIndex'])->name('add-category');
-//         Route::post('/submit', [CategoryController::class, 'InsertCategory']);
-//         Route::get('/categoryedit/{id}', [CategoryController::class, 'editCategory'])->name('edit-category');
-//         Route::post('edit-category/{category}', [CategoryController::class, 'updateCategory']);
-//         Route::get('/deletecategory/{category}', [CategoryController::class, 'deleteCategory'])->name('delete-category');
-//     });
-
-// /* Product Routes */
-//     Route::prefix('product')->group(function() {
-//         Route::get('/viewProduct',[ProductController::class,'productIndex'])->name('view-product');
-//         Route::get('/add-product', [ProductController::class,'AddProductForm'])->name('Add-Product-form');
-//         Route::post('/create-product', [ProductController::class,'CreateProduct'])->name('create-product');
-//         Route::get('/productedit/{product}', [ProductController::class, 'editProduct'])->name('edit-product');
-//         Route::put('/edit-product/{product}', [ProductController::class, 'updateProduct']);
-//         Route::get('/deleteproduct/{product}', [ProductController::class, 'deleteProduct'])->name('delete-product');
-//     });
-
 });
